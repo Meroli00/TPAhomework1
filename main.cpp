5d8696bb119a087a13a569f70d09bd6e852a79b0
Meroli00 <83272957+Meroli00@users.noreply.github.com>
2021-04-27 18:59:27 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+
+#include <MeroliWB.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+}
+
+/*
+ qui chiedi all'utente cosa intende produrre da terminale 
+ poi si inizializza il puntatore  alla struct WB 
+ per poi fare un while o if o simili per eseguire l'intenzione o comando
+ gli intenti sono 
+creare nuova bottiglia
+salvare il file come svg 
+cancellare file svg 
+caricare un file svg precedentemente creato
+uscire dal programma
+*/
+
+
